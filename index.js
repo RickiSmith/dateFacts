d9fed2b75c0400f77358e0ab885914bbe8826103
Ricki Smith <RickiS4@icloud.com>
2017-08-15 02:20:46 -0400

    final commit

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const chalk = require("chalk");
 
 console.log("It is " + chalk.blue(moment().format("LLLL"))+ ".");
 console.log("It is the " + chalk.magenta(moment().dayOfYear()+ "th") + " day of the year");
-console.log("It" + isTrue(moment().isDST) + "during Daylight Savings Time")
+
+console.log("It is " + chalk.cyan(moment().diff(moment().startOf("day"),"seconds"))+ " seconds into the day");
+console.log("It " + chalk.green(isTrue(moment().isDST())) + " during Daylight Savings Time");
+  console.log("It " + chalk.red(isTrue(moment().isLeapYear())) + " a leap year");
 
 function isTrue (boolValue){
    if (boolValue){
